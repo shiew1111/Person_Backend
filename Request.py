5e2d2a20ab4b1ab9ed53fc2e150b2d50723ce38a
shiew1111 <43846075+shiew1111@users.noreply.github.com>
2020-08-07 19:09:49 +0200

    Request modify.

--- Request.py
+++ Request.py
@@ -1,17 +1,10 @@
 import requests
 
 
-class Request:
-    def __init__(self):
-        # url to ask randomuser.me API for same set of persons like in attached persons.json file.
-        self.url = 'https://randomuser.me/api/?seed=abc&page=1&results=1000&version=1.3'
 
-        self.response = requests.request("GET", self.url)
+def get_people_from_api():
+    # url to ask randomuser.me API for same set of persons like in attached persons.json file.
+    url = 'https://randomuser.me/api/?seed=abc&page=1&results=1000&version=1.3'
 
-    def from_api(self):
-        return self.response.json().get('results')
-
-
-
-
-
+    response = requests.request("GET", url)
+    return response.json().get('results')
